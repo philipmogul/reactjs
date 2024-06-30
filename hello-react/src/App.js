32299ed6b63d91b81716666c63a3798fbeefe426
philipmogul <philipp1mogul@gmail.com>
2024-06-30 03:13:28 +0300

    ReactJs Day 1

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Hello />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-react/src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+    return(
+        <div>
+            <h3>Philip Mumo</h3><hr />
+            <h3>Cognoscenti Software Engineer!</h3>
+            <h4>&trade; &copy; 2024</h4>
+        </div>
+    )
+}
+
+export default Hello 
